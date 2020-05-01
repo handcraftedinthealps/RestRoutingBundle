2760494dffa91a043c8a04e104bedfb478b43d00
Alexander Schranz <alexander@sulu.io>
2020-05-01 17:12:03 +0200

    Update copyholders on the license

--- Tests/Routing/Loader/LoaderTest.php
+++ Tests/Routing/Loader/LoaderTest.php
@@ -3,10 +3,11 @@
 /*
  * This file is part of Handcrafted in the Alps - Rest Routing Bundle Project.
  *
- * (c) Sulu GmbH <hello@sulu.io>
+ * (c) 2011-2020 FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ * (c) 2020 Sulu GmbH <hello@sulu.io>
  *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace HandcraftedInTheAlps\RestRoutingBundle\Tests\Routing\Loader;
